d19d2a42287c222ed3a5cf398eb6eda11c611c02
Estifanos <estifanosbeyene21@gmail.com>
2023-03-25 18:56:03 -0400

    feat: remove unused functions

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -4,20 +4,7 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150))
-
-    @property
-    def serialize(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email,
-        }
-
-    @staticmethod
-    def get(user_id):
-        return User.query.get(user_id)
