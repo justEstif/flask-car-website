e3b7d05f29bb46a6a9b8789f9bdb18ee2533fef7
Estifanos <estifanosbeyene21@gmail.com>
2023-03-27 16:59:17 -0400

    feat: create one to many and m-> m relationship to cars table

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -3,12 +3,23 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+saved_cars = db.Table("saved_cars",
+                      db.Column('user_id', db.Integer, db.ForeignKey(
+                          'user.id'), primary_key=True),
+                      db.Column('car_id', db.Integer, db.ForeignKey(
+                          'car.id'), primary_key=True)
+                      )
+
+
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150))
     email = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(150))
+
+    cars = db.relationship('Car', backref='seller', lazy=True)
+    saved_cars = db.relationship(
+        "Car", secondary="saved_cars", backref="saved_by", lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
