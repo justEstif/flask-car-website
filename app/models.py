acb92e2d22aad42b1a4d2c0f2a0692b82e795e55
Estifanos <estifanosbeyene21@gmail.com>
2023-03-29 13:39:50 -0400

    feat: remove function from user model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import func
 from db import db
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
 
 saved_cars = db.Table("saved_cars",
                       db.Column('user_id', db.Integer, db.ForeignKey(
@@ -19,12 +18,6 @@ class User(db.Model, UserMixin):
     cars = db.relationship('Car', backref='seller', lazy=True)
     saved_cars = db.relationship(
         "Car", secondary="saved_cars", backref="saved_by", lazy=True)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
 
 
 class Car(db.Model):
