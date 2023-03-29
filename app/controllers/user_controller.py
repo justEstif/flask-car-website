d9d6fb4affe9cc071e52eb6b56fc9adc3a26cf1d
Estifanos <estifanosbeyene21@gmail.com>
2023-03-29 13:37:58 -0400

    feat: remove unsed imports

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
 user_bp = Blueprint("user_bp", __name__, url_prefix="/user")
